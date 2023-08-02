8c63a55bb1ff709b356662181cace09f4ae4caf2
r <r@DESKTOP-NIAIPDD.lan>
2023-08-02 11:26:53 -0400

    upgrade

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "AWS_REGION" {
     default = "us-east-1"
 }
 variable "credentials_file" {
-    default = "~/.aws/credentials"
+    default = "/var/lib/jenkins/credentials"
 }
 variable "aws_credentials_profile" {
     default = "dev"
